d94a5a87c5e52edf48ef81196c86feac3855c3bf
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:21:58 +0530

    create crop page

---
+++ src/pages/CropsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CropsPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Crops</h1>
+      <p>Manage your crops here.</p>
+    </div>
+  );
+};
+
+export default CropsPage;
