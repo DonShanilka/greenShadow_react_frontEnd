8945f8977444e81381de0892c0eeaeff4205a392
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:24:32 +0530

    create FieldPage

---
+++ src/pages/FieldPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FieldPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Field</h1>
+      <p>View and edit field data here.</p>
+    </div>
+  );
+};
+
+export default FieldPage;
