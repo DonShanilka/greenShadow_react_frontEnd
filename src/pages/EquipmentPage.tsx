dbbfdf82a3bc78209d78c26df2fbbaaa7c718c4b
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:39:28 +0530

    create EquipmentPage

---
+++ src/pages/EquipmentPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EquipmentPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Equipment</h1>
+      <p>Manage your equipment details here.</p>
+    </div>
+  );
+};
+
+export default EquipmentPage;
