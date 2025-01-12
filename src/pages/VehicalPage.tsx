7fa4e87364a19ecb9120e45ee48c2bfe7c6df71c
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:39:00 +0530

    create VehicalPage

---
+++ src/pages/VehicalPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VehicalPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Vehical</h1>
+      <p>Keep track of vehicals here.</p>
+    </div>
+  );
+};
+
+export default VehicalPage;
