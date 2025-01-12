e15d8e2e2cdc6fe9e52b76b7bfa8eda58216f2fa
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:38:14 +0530

    create StaffPage

---
+++ src/pages/StaffPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StaffPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Staff</h1>
+      <p>Manage your staff information here.</p>
+    </div>
+  );
+};
+
+export default StaffPage;
