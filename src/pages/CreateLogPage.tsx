c207deac79b612c0e1e02b21e329828362c69689
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:40:13 +0530

    create CreateLogPage

---
+++ src/pages/CreateLogPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateLogPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Create Log</h1>
+      <p>Create logs for your system here.</p>
+    </div>
+  );
+};
+
+export default CreateLogPage;
