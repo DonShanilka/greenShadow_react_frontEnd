37128732a461103f0418cc22955181e8cce21eb6
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:40:45 +0530

    create AddLogPage

---
+++ src/pages/AddLogPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddLogPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Add Log</h1>
+      <p>Add new log entries here.</p>
+    </div>
+  );
+};
+
+export default AddLogPage;
