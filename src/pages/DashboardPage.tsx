af2986ff8066162c25686da0ce697223ea9d79b4
Shanilka800@# <shanilka799@gmail.com>
2025-01-12 12:08:03 +0530

    create DashboardPage

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p>Welcome to the dashboard page!</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
