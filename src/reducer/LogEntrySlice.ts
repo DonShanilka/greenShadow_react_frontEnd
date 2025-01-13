1e9f005ad475501a48dd1e6fe567b5505b3e40f4
Shanilka800@# <shanilka799@gmail.com>
2025-01-13 19:56:18 +0530

    add LogEntrySlice body

---
+++ src/reducer/LogEntrySlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const LogEntrySlice = createSlice({
+  name:'logs',
+  initialState: initialState,
+  reducers: {
+    addLog: (state, action) => {
+      state.push(action.payload);
+      // console.log(addCrop);
+    },
+    updateLog: (state, action) => {
+      const index = state.findIndex(log => log.logId === action.payload.logId);
+      if (index !== -1) {
+        state[index] = {
+          ...state[index],
+          ...action.payload,
+        };
+      }
+    },
+    deleteLog: (state,action) => {
+      return state.filter((log) => log.logId !== action.payload.logId);
+    },
+  },
+})
+
+export const {addLog, updateLog, deleteLog} = LogEntrySlice.actions;
+export default LogEntrySlice.reducer
