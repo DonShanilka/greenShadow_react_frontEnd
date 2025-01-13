503b613a139216446e5b4cd39803383b93a141f3
Shanilka800@# <shanilka799@gmail.com>
2025-01-13 20:03:49 +0530

    update store

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cropReducer from '../reducer/CropSlice';
 import equipmentReducer from '../reducer/EquipmentSlice';
-
+import fieldReducer from '../reducer/FieldSlice'
+import staffReducer from '../reducer/StaffSlice'
+import vehicalReducer from '../reducer/VehicalSlice'
+import logReducer from '../reducer/LogEntrySlice'
 
 export const store = configureStore({
   reducer: {
     crops: cropReducer,
+    fields: fieldReducer,
     equipments: equipmentReducer,
+    staffs: staffReducer,
+    vehicles: vehicalReducer,
+    logs: logReducer
   },
 });
 
