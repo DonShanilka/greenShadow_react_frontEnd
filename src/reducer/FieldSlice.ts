07be25777f9a275fe905b208c7647756fae621a8
Shanilka800@# <shanilka799@gmail.com>
2025-01-13 19:32:22 +0530

    add FieldSlice body

---
+++ src/reducer/FieldSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const FieldSlice = createSlice({
+  name:'fields',
+  initialState: initialState,
+  reducers: {
+    addField: (state, action) => {
+      state.push(action.payload);
+      // console.log(addCrop);
+    },
+    updateField: (state, action) => {
+      const index = state.findIndex(field => field.fieldId === action.payload.fieldId);
+      if (index !== -1) {
+        state[index] = {
+          ...state[index],
+          ...action.payload,
+        };
+      }
+    },
+    deleteField: (state,action) => {
+      return state.filter((field) => field.fieldId !== action.payload.fieldId);
+    },
+  },
+})
+
+export const {addField, updateField, deleteField} = FieldSlice.actions;
+export default FieldSlice.reducer
