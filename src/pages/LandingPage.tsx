57c7f1d735728600d16ce2db7bd224fe4548c149
Shanilka800@# <shanilka799@gmail.com>
2025-01-17 23:11:24 +0530

    create landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+      <div className='mainDiv'>
+        <div className=''>
+          <img  src="src\assets\image\landing\Sustainable Agriculture Meets AI_ The Rike's Green Revolution Project.jpg" alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage
