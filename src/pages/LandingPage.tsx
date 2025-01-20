30caab18e93b84f557a9dcfe9dd89175ec26adeb
Shanilka800@# <shanilka799@gmail.com>
2025-01-20 19:43:06 +0530

    update landing page css

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,19 +4,19 @@ import '../assets/css/LandingPage.css'
 const LandingPage = () => {
   return (
     <div className="mainDiv">
-      <img src="src\assets\image\landing\treeImage.png" alt="" className='treeImage'/>
+      <img src="src\assets\image\landing\GreenShadowLogo.png" alt="" className='treeImage'/>
       <div className='green-div'></div>
-      <div className="textDiv max-w-7xl mx-auto text-center">
+      <div className="textDiv max-w-7xl mx-auto text-left">
         
-        <h2 className="text-6xl font-bold text-white mb-6">
-          Welcome to Green Shadow
+        <h2 className="text-8xl font-bold text-white mb-6">
+          Welcome to <br /> Green Shadow
         </h2>
-        {/* <p className="text-lg text-green-500">
+        <p className="text-2xl text-green-500">
           Green Shadow is your trusted crop monitoring system, designed to help
           you efficiently manage your fields and crops. Track growth progress,
           analyze field data, and optimize your agricultural productivity all in
           one place.
-        </p> */}
+        </p>
         <div className="mt-8 mb-6">
           <a
             href="/get-started"
